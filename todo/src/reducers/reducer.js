9d1bb9c1be0a6c2db78e95b72febfdd511dcdb4c
Victoria  Topham <topham.victoria@gmail.com>
2020-04-13 10:18:01 -0700

    got a reducer function and initial state displayed

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export const initialState = [{
+    item: 'Lean about reducers',
+    completed: false,
+    id: 3892987589
+},
+{
+    item: 'Eat some veggies',
+    completed: true,
+    id: 2984029834
+}]
+export const reducer = (state, action) => {
+    return []
+}
